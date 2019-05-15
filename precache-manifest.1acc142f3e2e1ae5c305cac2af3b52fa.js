0fac1546e4a44745ae5d0a0d6d5baf6c03e37cbc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-15 14:56:32 +0000

    Deploy monochromer/chart-demo to github.com/monochromer/chart-demo.git:gh-pages

---
+++ precache-manifest.1acc142f3e2e1ae5c305cac2af3b52fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f0a8b1d0513fb99baea",
+    "url": "/chart-demo/static/js/runtime~main.9f0a8b1d.js"
+  },
+  {
+    "revision": "aafca714812e03f61d58",
+    "url": "/chart-demo/static/js/main.aafca714.chunk.js"
+  },
+  {
+    "revision": "8d069526816891932c16",
+    "url": "/chart-demo/static/js/1.8d069526.chunk.js"
+  },
+  {
+    "revision": "aafca714812e03f61d58",
+    "url": "/chart-demo/static/css/main.40210726.chunk.css"
+  },
+  {
+    "revision": "058522647a38056c34ea6abeb5608be7",
+    "url": "/chart-demo/index.html"
+  }
+];
